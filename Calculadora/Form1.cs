80bc0ece72c83a38185c7c37025a911652464cbe
Wadrian Araujo De Moura <wadm.snf20@uea.edu.br>
2022-12-13 08:27:57 -0400

    Initial commit of the best calculator of world
    Calculator created to improve my C# skills

---
+++ Calculadora/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
